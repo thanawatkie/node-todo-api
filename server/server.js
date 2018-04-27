20a8d6d7139262b77c404f695cb2db8e717e46d3
thanawatkie <thanwat.kie@student.mahidol.ac.th>
2018-04-27 23:49:42 +0700

    add Patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const _ = require('lodash');
 var {mongooes} = require('./db/mongoose');
 var {Todo} = require('./model/todo');
 var {user} = require('./model/user');
@@ -64,6 +64,36 @@ app.delete('/todos/:id', (req,res) => {
     }
 });
 
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    else{
+        if(_.isBoolean(body.completed) && body.completed) {
+            body.completedAt = new Date().getTime();
+        }else{
+            body.completed = false;
+            body.completedAt = null;
+        }
+
+        Todo.findByIdAndUpdate(id, {$set: {
+            text: body.text,
+            completed: body.completed,
+            completedAt: body.completedAt
+        }}, {new: true}).then((todo) => {
+            if(!todo){
+                res.status(404).send();
+            }else{
+                res.send({todo});
+            }
+        }).catch((err) => {
+            res.status(400).send();
+        });
+    }
+})
 
 var port = process.env.PORT || 3000;
 app.listen( port, () => {
