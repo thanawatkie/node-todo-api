afe0e69db12b9effc38823fb25d6924db672811f
thanawatkie <thanwat.kie@student.mahidol.ac.th>
2018-04-28 10:50:55 +0700

    set up test and development env

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'devlopment';
 
 console.log('env ********',env);
 
 
-if(env === 'development'){
+if(env === 'dev'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
 } else if(env === 'test'){
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongooes = require('mongoose');
 
 mongooes.Promise = global.Promise;
-mongooes.connect('mongodb://localhost:27017/TodoApp');
-// mongooes.connect('mongodb://node-web-rest:61Fb4YGJUqJ8jFjfSvxlU7wjlLrHZg4HsX4FvZNxy5Rd3TeznzEu2Ihparz4mX346KZEFM5bs3nizPQIrzT9CA%3D%3D@node-web-rest.documents.azure.com:10255/?ssl=true');
+// mongooes.connect('mongodb://localhost:27017/TodoApp');
+mongooes.connect('mongodb://node-web-rest:61Fb4YGJUqJ8jFjfSvxlU7wjlLrHZg4HsX4FvZNxy5Rd3TeznzEu2Ihparz4mX346KZEFM5bs3nizPQIrzT9CA%3D%3D@node-web-rest.documents.azure.com:10255/?ssl=true');
 
 module.exports = {mongooes};
